2f35543479f0f232c1ea21e0f95a598b8218890a
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2019-03-14 18:47:12 -0300

    Creates an initial structure for card component

---
+++ components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+import styled from 'styled-components/native'
+
+const CardView = styled.View`
+  color: red;
+  background-color: #eeeeee;
+  padding: 20px;
+  margin: 0 15px 20px;
+  border-radius: 7;
+  box-shadow: 10px 10px 10px rgba(0,0,0,1);
+`
+const CardQuestion = styled.Text`
+  color: #111;
+  font-size: 18px;
+  margin-bottom: 15px;
+`
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <CardView>
+          <CardQuestion style={{ textAlign: 'center' }}>Is this working?</CardQuestion>
+          <Button title='SIM' color='green' onPress={() => console.log('wow')} />
+          <View style={{ marginBottom: 10 }} />
+          <Button title='NÃO' color='red' onPress={() => alert('wow')} />
+        </CardView>
+        <CardView>
+          <CardQuestion style={{ textAlign: 'center' }}>Is this working?</CardQuestion>
+          <Button title='SIM' color='green' onPress={() => console.log('wow')} />
+          <View style={{ marginBottom: 10 }} />
+          <Button title='NÃO' color='red' onPress={() => alert('wow')} />
+        </CardView>
+      </View>
+    );
+  }
+}
